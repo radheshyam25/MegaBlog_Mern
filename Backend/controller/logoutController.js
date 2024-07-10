818cb8d8b9cd7cdaf5d0f308ca261dce3383e933
radheshyam25 <147301200+radheshyam25@users.noreply.github.com>
2024-07-11 00:21:44 +0530

    Update logoutController.js

--- Backend/controller/logoutController.js
+++ Backend/controller/logoutController.js
@@ -2,7 +2,7 @@ const User=require('../model/User');
 
 const handleLogout=async(req,res)=>{
     const cookies=req.cookies;
-    if(!cookies?.jwt) return res.status(401).json(req);
+    if(!cookies?.jwt) return res.status(401).json({"message": req.cookies});
     const refreshtoken=cookies.jwt;
 
     const founduser=await User.findOne({refreshtoken:refreshtoken}).exec();
